0a8838fb2112c710f8546cf3671319b7bd1ca8c7
Ray Perea <ray@consta.link>
2026-03-22 14:20:37 -0500

    Update the StreamType signature to include the stream name

--- headers/StreamType.h
+++ headers/StreamType.h
@@ -5,6 +5,7 @@
  * This function allocates a new typeType value from the values passed in
  * and tracks it on the current memory stack
  *
+ * @param name       A pointer to the stream name - This should be the same as the stream ID
  * @param paramType  A pointer to the open parameter type that the stream expects when opening the stream
  * @param outputType A pointer to the output data type that the stream outputs when being read from
  * @param inputType  A pointer to the input data type that the stream expects when being written to
@@ -29,6 +30,7 @@
 // Included types
 //-------------------------------------------------------------------------------------
 #include "typeBool.h"
+#include "typeData.h"
 #include "typeType.h"
 
 //-------------------------------------------------------------------------------------
@@ -39,6 +41,7 @@
 // Header definition
 //-------------------------------------------------------------------------------------
 typeType* StreamType(
+	typeData* name,
 	typeType* paramType,
 	typeType* outputType,
 	typeType* inputType,
